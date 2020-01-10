7cc96426fcc143aea9361dcc352175b41569f92d
Ashish Bakshi <38886700+ashishaof@users.noreply.github.com>
2020-01-10 22:36:55 +0100

    Update list_helper.js

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -10,7 +10,7 @@ const totalLikes = (blogs) => {
 
 const favoriteBlog = (blogs) => {
   const topBlog = blogs.reduce((a, c) => c.likes > a.likes ? c : a)
-  // Omit the the 3 properties '_id', 'url' and '__v'
+ 
   const { _id, url, __v, ...topBlog3 } = topBlog
   return topBlog3
 }
